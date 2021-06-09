7ac1a697de5815ee339718ab2638a342c414640c
Diogenes <diogensgreen@gmail.com>
2021-06-09 00:05:04 -0300

    refactor: recriando a tela de Feeds

--- vivit-tomato/src/screens/Feed/feed.tsx
+++ vivit-tomato/src/screens/Feed/feed.tsx
@@ -1,186 +1,242 @@
-import React from 'react'
+import * as React from 'react'
 import {
-  View,
-  ActivityIndicator,
-  Animated,
+  StatusBar,
   Image,
-  SafeAreaView,
-  Dimensions
+  FlatList,
+  Dimensions,
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView
 } from 'react-native'
-
-import { useQuery } from '@apollo/client'
-import * as S from './styles'
-
-import { QueryPosts } from 'graphql/generated/QueryPosts'
-import { QUERY_POSTS } from '../../graphql/queries/posts'
-import { StatusBar } from 'react-native'
-
+const { width } = Dimensions.get('screen')
+import { EvilIcons } from '@expo/vector-icons'
 import {
-  Directions,
   FlingGestureHandler,
-  State,
-  TouchableOpacity
+  Directions,
+  State
 } from 'react-native-gesture-handler'
 
-import Text from '../../components/Text'
+import { useQuery } from '@apollo/client'
+import { QueryPosts } from 'graphql/generated/QueryPosts'
+import { QUERY_POSTS } from '../../graphql/queries/posts'
+import theme from '../../styles/theme'
 
-import { SharedElement } from 'react-navigation-shared-element'
-import { useNavigation, useRoute } from '@react-navigation/native'
+const OVERFLOW_HEIGHT = 90
+const SPACING = 10
+const ITEM_WIDTH = width * 0.86
+const ITEM_HEIGHT = ITEM_WIDTH * 1.5
+const VISIBLE_ITEMS = 3
 
-const Feeds = ({ navigation }) => {
-  const IMAGE_WIDTH = Dimensions.get('window').width * 0.86
-  const IMAGE_HEIGHT = IMAGE_WIDTH * 1.5
-  const VISIBLE_ITEMS = 4
+const OverflowItems = ({ data, scrollXAnimated }) => {
+  const inputRange = [-1, 0, 1]
+  const translateY = scrollXAnimated.interpolate({
+    inputRange,
+    outputRange: [OVERFLOW_HEIGHT, 0, -OVERFLOW_HEIGHT]
+  })
+  return (
+    <View style={styles.overflowContainer}>
+      <Animated.View style={{ transform: [{ translateY }] }}>
+        {data?.posts.map((item, index) => {
+          return (
+            <View key={index} style={styles.itemContainer}>
+              <Text style={[styles.title]} numberOfLines={1}>
+                {item.title}
+              </Text>
+              <View style={styles.itemContainerRow}>
+                <Text style={[styles.location]}>
+                  <EvilIcons
+                    name="location"
+                    size={16}
+                    color="white"
+                    style={{ marginRight: 5 }}
+                  />
+                  {item?.centro.name}
+                </Text>
+                <Text style={[styles.date]}>{item.date}</Text>
+              </View>
+            </View>
+          )
+        })}
+      </Animated.View>
+    </View>
+  )
+}
 
-  const { data, error, loading } = useQuery<QueryPosts>(QUERY_POSTS)
+export default function App() {
+  /* const [data, setData] = React.useState(DATA) */
+  const scrollXIndex = React.useRef(new Animated.Value(0)).current
+  const scrollXAnimated = React.useRef(new Animated.Value(0)).current
 
-  if (error) {
-    console.log('Error fetching posts', error)
+  const [index, setIndex] = React.useState(0)
+  const [refreshing, setRefreshing] = React.useState(false)
+  const [total, setTotal] = React.useState(0)
+
+  const setActiveIndex = React.useCallback((activeIndex) => {
+    scrollXIndex.setValue(activeIndex)
+    setIndex(activeIndex)
+  })
+
+  const { data, error, loading, fetchMore, refetch } = useQuery<QueryPosts>(
+    QUERY_POSTS
+  )
+
+  async function loadPage(pageNumber = 2, shouldRefresh = false) {}
+
+  async function refreshList() {
+    refetch({
+      query: QUERY_POSTS
+    })
   }
 
-  const [activeIndex, setActiveIndex] = React.useState(0)
-  const animatedValue = React.useRef(new Animated.Value(0)).current
-  const reactiveAnimated = React.useRef(new Animated.Value(0)).current
+  React.useEffect(() => {
+    loadPage()
+  }, [])
 
   React.useEffect(() => {
-    Animated.timing(animatedValue, {
-      toValue: reactiveAnimated,
-      duration: 300,
+    Animated.spring(scrollXAnimated, {
+      toValue: scrollXIndex,
       useNativeDriver: true
     }).start()
-  }, [reactiveAnimated, animatedValue])
-
-  const setActiveSlide = React.useCallback((newIndex) => {
-    setActiveIndex(newIndex)
-    reactiveAnimated.setValue(newIndex)
   })
 
   return (
     <FlingGestureHandler
-      key="UP"
-      direction={Directions.UP}
-      onHandlerStateChange={(event) => {
-        if (event.nativeEvent.state === State.END) {
-          // incremente index
-          if (activeIndex === data.posts.length - 1) {
+      key="left"
+      direction={Directions.LEFT}
+      onHandlerStateChange={(ev) => {
+        if (ev.nativeEvent.state === State.END) {
+          if (index === data?.posts.length - 1) {
             return
           }
-          setActiveSlide(activeIndex + 1)
-          /* console.log('=>', activeIndex + 1) */
+          setActiveIndex(index + 1)
         }
       }}
     >
       <FlingGestureHandler
-        key="DOWN"
-        direction={Directions.DOWN}
-        onHandlerStateChange={(event) => {
-          if (event.nativeEvent.state === State.END) {
-            // incremente index
-            if (activeIndex === 0) {
+        key="right"
+        direction={Directions.RIGHT}
+        onHandlerStateChange={(ev) => {
+          if (ev.nativeEvent.state === State.END) {
+            if (index === 0) {
               return
             }
-            setActiveSlide(activeIndex - 1)
-            /*  console.log(activeIndex - 1) */
+            setActiveIndex(index - 1)
           }
         }}
       >
-        <SafeAreaView style={{ flex: 1 }}>
-          <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-          {loading ? (
-            <ActivityIndicator color="#333" />
-          ) : (
-            <Animated.FlatList
-              data={data.posts}
-              scrollEnabled={false}
-              horizontal
-              inverted
-              keyExtractor={(item) => item.id}
-              contentContainerStyle={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
-              }}
-              CellRendererComponent={({
-                index,
-                item,
-                children,
-                style,
-                ...props
-              }) => {
-                const newStyles = [
-                  style,
-                  {
-                    zIndex: data.posts.length + index,
-                    left: -IMAGE_WIDTH / 2,
-                    top: -IMAGE_HEIGHT / 2
-                  }
-                ]
-                return (
-                  <View key={index} {...props} style={newStyles}>
-                    {children}
-                  </View>
-                )
-              }}
-              renderItem={({ item, index }) => {
-                const inputRange = [index - 1, index, index + 1]
-                console.log('=>', inputRange)
-                const translateY = animatedValue.interpolate({
-                  inputRange,
-                  outputRange: [-40, 0, 30]
-                })
-                const opacity = animatedValue.interpolate({
-                  inputRange,
-                  outputRange: [1 - 1 / VISIBLE_ITEMS, 1, 0]
-                })
-                const scale = animatedValue.interpolate({
-                  inputRange,
-                  outputRange: [0.92, 1, 1.3]
-                })
+        <SafeAreaView style={styles.container}>
+          <FlatList
+            data={data?.posts}
+            keyExtractor={(_, index) => String(index)}
+            horizontal
+            onRefresh={refreshList}
+            refreshing={refreshing}
+            contentContainerStyle={{
+              flex: 1,
+              justifyContent: 'center',
+              padding: SPACING * 2,
+              marginTop: 50
+            }}
+            scrollEnabled={false}
+            removeClippedSubviews={false}
+            CellRendererComponent={({
+              item,
+              index,
+              children,
+              style,
+              ...props
+            }) => {
+              const newStyle = [style, { zIndex: data.posts.length - index }]
+              return (
+                <View style={newStyle} index={index} {...props}>
+                  {children}
+                </View>
+              )
+            }}
+            renderItem={({ item, index }) => {
+              const inputRange = [index - 1, index, index + 1]
+              const translateX = scrollXAnimated.interpolate({
+                inputRange,
+                outputRange: [50, 0, -100]
+              })
+              const scale = scrollXAnimated.interpolate({
+                inputRange,
+                outputRange: [0.8, 1, 1.6]
+              })
+              const opacity = scrollXAnimated.interpolate({
+                inputRange,
+                outputRange: [1 - 1 / VISIBLE_ITEMS, 1, 0]
+              })
 
-                return (
-                  <Animated.View
+              return (
+                <Animated.View
+                  style={{
+                    position: 'absolute',
+                    left: -ITEM_WIDTH / 2,
+                    opacity,
+                    transform: [
+                      {
+                        translateX
+                      },
+                      { scale }
+                    ]
+                  }}
+                >
+                  <Image
+                    source={{ uri: `http://5.183.8.1:1337${item?.cover?.url}` }}
                     style={{
-                      position: 'absolute',
-                      opacity,
-                      transform: [{ translateY }, { scale }]
+                      width: ITEM_WIDTH,
+                      height: ITEM_HEIGHT,
+                      borderRadius: 14
                     }}
-                  >
-                    <TouchableOpacity
-                      onPress={() => {
-                        navigation.navigate('Details', { item: item.id })
-                      }}
-                    >
-                      <SharedElement id={`item.${item.id}.photo`}>
-                        <Image
-                          source={{
-                            uri: `http://5.183.8.1:1337${item?.cover?.url}`
-                          }}
-                          style={{
-                            width: IMAGE_WIDTH,
-                            height: IMAGE_HEIGHT,
-                            borderRadius: 20,
-                            resizeMode: 'cover'
-                          }}
-                        />
-                      </SharedElement>
-
-                      <View style={{ position: 'absolute', bottom: 20 }}>
-                        <Text
-                          size="xxxlarge"
-                          text={item.title}
-                          uppercase={true}
-                        />
-                      </View>
-                    </TouchableOpacity>
-                  </Animated.View>
-                )
-              }}
-            />
-          )}
+                  />
+                </Animated.View>
+              )
+            }}
+          />
+          <OverflowItems data={data} scrollXAnimated={scrollXAnimated} />
         </SafeAreaView>
       </FlingGestureHandler>
     </FlingGestureHandler>
   )
 }
 
-export default Feeds
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: theme.theme_colors.back
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: '900',
+    textTransform: 'uppercase',
+    letterSpacing: -1,
+    fontFamily: theme.font.family.ubuntu_bold,
+    color: theme.theme_colors.white
+  },
+  location: {
+    fontSize: 16,
+    color: theme.theme_colors.white
+  },
+  date: {
+    fontSize: 12,
+    color: theme.theme_colors.white
+  },
+  itemContainer: {
+    height: OVERFLOW_HEIGHT,
+    padding: SPACING * 2
+  },
+  itemContainerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  overflowContainer: {
+    marginBottom: 20,
+    height: OVERFLOW_HEIGHT,
+    overflow: 'hidden'
+  }
+})
