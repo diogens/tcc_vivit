dfe30cee6a07b1e9be351ef9a425724e40da02de
Diogenes <diogensgreen@gmail.com>
2021-06-13 23:10:01 -0300

    fix: change server

--- vivit-tomato/apollo.ts
+++ vivit-tomato/apollo.ts
@@ -1,8 +1,13 @@
 import React from 'react'
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  createHttpLink
+} from '@apollo/client'
 import { NormalizedCacheObject } from '@apollo/client/cache/inmemory/types'
 import { AsyncStorage } from 'react-native'
-import { setContext } from '@apollo/link-context'
+import { setContext } from '@apollo/client/link/context'
 
 /*
 uncomment the code below in case you are using a GraphQL API that requires some form of
@@ -19,24 +24,25 @@ const asyncAuthLink = setContext(async () => {
 await AsyncStorage.getItem('@CofferIsland:username')
 */
 
+/* const GRAPHQL_API_URL = 'http://192.168.1.70:1337/graphql' */
 const GRAPHQL_API_URL = 'http://5.183.8.1:1337/graphql'
 
-const asyncAuthLink = setContext(async () => {
-  const TOKEN = await AsyncStorage.getItem('@CofferIsland:username')
-  console.log('### =>=>', TOKEN)
+const asyncAuthLink = setContext(async (_, { headers }) => {
+  const TOKEN = await AsyncStorage.getItem('@CofferIsland:jwt')
+  console.log('#### =>=>2', headers)
   return {
     headers: {
-      Authorization: TOKEN
+      Authorization: TOKEN ? `Bearer ${JSON.parse(TOKEN)}` : ``
     }
   }
 })
 
-const httpLink = new HttpLink({
+const httpLink = createHttpLink({
   uri: GRAPHQL_API_URL
 })
 
 export const apolloClient = new ApolloClient<NormalizedCacheObject>({
-  /* link: asyncAuthLink.concat(httpLink), */
-  link: httpLink,
+  link: asyncAuthLink.concat(httpLink),
   cache: new InMemoryCache()
 })
+/* link: httpLink, */
