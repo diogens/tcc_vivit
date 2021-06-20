720e44314351e105603b921d1ae911b51057de0f
Diogenes <diogensgreen@gmail.com>
2021-06-20 01:57:44 -0300

    fix userform hook

--- vivit-tomato/src/hooks/useForms.ts
+++ vivit-tomato/src/hooks/useForms.ts
@@ -54,14 +54,6 @@ const mask = {
       .replace(/(\d{3})(\d{1,2})/, '$1-$2')
       .replace(/(-\d{2})\d+?$/, '$1')
   },
-  cnpj(value) {
-    return value
-      .replace(/\D/g, '')
-      .replace(/(\d{3})(\d)/, '$1.$2')
-      .replace(/(\d{3})(\d)/, '$1.$2')
-      .replace(/(\d{3})(\d{1,2})/, '$1-$2')
-      .replace(/(-\d{2})\d+?$/, '$1')
-  },
   telefone(value) {
     return value
       .replace(/\D/g, '')
